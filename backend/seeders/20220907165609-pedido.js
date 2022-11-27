a02b481f02aba111a02101f071260ade1deab1ac
Henrique Caron <henriquecaron03@gmail.com>
2022-11-27 20:47:02 -0300

    Inserido novos pedidos e produtos

--- backend/seeders/20220907165609-pedido.js
+++ backend/seeders/20220907165609-pedido.js
@@ -29,7 +29,62 @@ module.exports = {
         nome_cliente: 'Henrique Caron',
         createdAt: new Date(),
         updatedAt: new Date()
-      }
+      },
+      {
+        observacao: 'Sem molhos',
+        quantidade_item: 2,
+        valor_total: 10,
+        item_id: 1,
+        nome_cliente: 'Caron',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        observacao: 'Com molho extra',
+        quantidade_item: 2,
+        valor_total: 15,
+        item_id: 2,
+        nome_cliente: 'Caron',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        observacao: 'Sem Alface',
+        quantidade_item: 4,
+        valor_total: 12,
+        item_id: 3,
+        nome_cliente: 'Caron',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        observacao: ' ',
+        quantidade_item: 1,
+        valor_total: 12.5,
+        item_id: 3,
+        nome_cliente: 'Caron',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        observacao: 'Favor levar guardanapos extra',
+        quantidade_item: 3,
+        valor_total: 14,
+        item_id: 2,
+        nome_cliente: 'Caron',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        observacao: 'Favore retirar celoba',
+        quantidade_item: 5,
+        valor_total: 25,
+        item_id: 1,
+        nome_cliente: 'Caron',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      
     ], {});
   },
 
