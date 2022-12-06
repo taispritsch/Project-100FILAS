6bdd5c7d376134dab3062a3bed47d8f84abbaad7
taispritsch <tais.pritsch@universo.univates.br>
2022-12-05 23:44:18 -0300

    Ajustes Carrinho

--- frontend/src/pages/carrinho/carrinho.js
+++ frontend/src/pages/carrinho/carrinho.js
@@ -1,13 +1,5 @@
 var orderNumber = JSON.parse(sessionStorage.getItem('numeroPedido'));
-console.log(orderNumber);
-
-// USAR ESSA VARIAVEL ORDERNUMBER PARA:
-/*
-1 - CHAMAR A ROTA DE PEDIDOS POR NUMÉRO_PEDIDO;
-OBS: A GABI AINDA IRÁ CRIAR ESSA ROTA.
-
-2 - RENDERIZAR OS ITENS ATRAVÉS DE UM MAP;
-*/
+//console.log(orderNumber);
 
 async function getPedido(id) {
     let url = 'http://localhost:8080/listarPedidoPorNumeroPedido/'+id;
@@ -19,42 +11,76 @@ async function getPedido(id) {
     }
 }
 
-/* async function getItem(id) {
-    let url = 'http://localhost:8080/listarPedido_has_item/'+id;
+//listar itens por estabelecimento
+async function getProducts(id) {
+    let url = 'http://localhost:8080/listarItensPorEstabelecimento/'+id;
     try {
         let res = await fetch(url);
         return await res.json();
     } catch (error) {
         console.error(error);
     }
-} */
+}
 
-
+let itens;
 let pedido;
 // Carrega o pedido;
 async function renderPedido() {
       pedido = [];
-    
-      pedido = await getPedido(orderNumber);;
+      itens = [];
+
+      pedido = await getPedido(orderNumber);
       
+      let nomeItem = '';
       let nomeCliente = '';
       let total = 0;
       let htmlTitle = ``;
       let html = '';
-      pedido.pedido.map(pedido => {
+      let htmlSegment = '';
 
+      pedido.pedido.map(async pedido => {
+
+        //imprime o nome do cliente na tela
         nomeCliente = pedido.nome_cliente;
         htmlTitle = `<p class="title">${nomeCliente.toUpperCase()}, SEU PEDIDO FOI EFETUADO COM SUCESSO!</p>`;
-        
-        total += pedido.valor_total;
-         let htmlSegment = `
-                
-                <div class="info">                    
-                    <p class="amount">${pedido.quantidade_item}x</p>
-                    <p class="total">R$${pedido.valor_total}</p>                
-                </div>`;
 
-         html += htmlSegment;
+        //valor total do pedido
+        total += pedido.valor_total;
+        
+        //pegar o nome do produto. 
+        itens = await getProducts(pedido.estabelecimento_id);
+        itens.itens.map(itens =>{
+            if(pedido.item_id == itens.id){
+                nomeItem = itens.nome;
+            }
+        });
+
+        //grava o pedido no array
+        let gravar = [{
+            'nome': nomeItem,
+            'quantidade': pedido.quantidade_item,
+            'valorTotal': pedido.valor_total,
+        }]
+
+        
+        gravar.forEach(item => {
+
+            htmlSegment = `
+                
+            <div class="list">
+                <div class="info">                   
+                    <p class="amount">${item.quantidade}x</p><p class="item">${item.nome}</p>          
+                </div>
+                <p class="totalItem">R$${item.valorTotal}</p>
+            </div>`;
+
+            
+           html += htmlSegment;
+        });
+
+        //imprime no carrinho
+        let container = document.querySelector('.card-pedido-container');
+        container.innerHTML = html;
       });
 
       let htmlValue = `
@@ -62,12 +88,9 @@ async function renderPedido() {
         <p class="total">TOTAL:</p>
         <p class="value-total">R$${total}</p>`;
 
-
       let title = document.querySelector('.title');
-      let container = document.querySelector('.card-pedido-container');
       let content = document.querySelector('.value');
       title.innerHTML = htmlTitle;
-      container.innerHTML = html;
       content.innerHTML = htmlValue;
 }    
 renderPedido();
